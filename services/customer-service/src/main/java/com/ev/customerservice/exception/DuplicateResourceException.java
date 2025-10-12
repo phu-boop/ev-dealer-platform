87ce8580dc631feec58290ef24a10df83276ce8a
tris1404 <nguyentaitri1442005@gmail.com>
2025-10-12 12:22:27 +0700

    refactor: Standardize packages & fix security for dev

--- services/customer-service/src/main/java/com/ev/customerservice/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.ev.customerservice.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
