de33f25a736677a81f8e90974e510ee02fc3e273
BinhLN1105 <luubinhbeo@gmail.com>
2025-10-12 20:50:04 +0700

    Fix vehicle-service/src/test

--- services/vehicle-service/src/test/java/com/ev/vehicle_service/VehicleServiceApplicationTests.java
+++ services/vehicle-service/src/test/java/com/ev/vehicle_service/VehicleServiceApplicationTests.java
@@ -1,16 +1,11 @@
 package com.ev.vehicle_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration; 
 import com.ev.vehicle_service.util.DotenvTestInitializer; 
 
 
-@SpringBootApplication(scanBasePackages = {
-    "com.ev.vehicle_service", 
-    "com.ev.common_lib"       
-})
 @SpringBootTest
 @ContextConfiguration(initializers = DotenvTestInitializer.class) 
 class VehicleServiceApplicationTests {
