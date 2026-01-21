a8de425fac214218fdfa931ac953b956fa7a279b
TrongDang2608 <zeusgamez2608@gmail.com>
2026-01-21 19:51:25 +0700

    Feature: Add Khách Hàng & Danh Mục Page for Admin 5174

--- gateway/src/main/java/com/ev/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/ev/gateway/config/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
 
                                 // "/sales/**"
                                 "/sales/**",
+                                "/orders/**",
 
                                 // vehicle-service
                                 "/vehicles/**",
